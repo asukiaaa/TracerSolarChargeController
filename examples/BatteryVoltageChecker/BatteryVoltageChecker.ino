0c3fab51d43632ef4e79e1acb4ac3d8d2111e7bb
Asuki Kono <asukiaaa@gmail.com>
2016-11-25 16:14:22 +0900

    Add example project

---
+++ examples/BatteryVoltageChecker/BatteryVoltageChecker.ino
@@ -0,0 +1,26 @@
+#include "TracerSolarChargeController.h"
+
+TracerSolarChargeController chargeController(10, 11); // RX, TX
+
+void setup() {
+  Serial.begin(57600);
+}
+
+void loop() {
+  if (chargeController.update()) {
+    chargeController.printInfo(&Serial);
+
+    if ( chargeController.battery > 26.3 ) {
+      Serial.println('Battery voltage is high!');
+    } else if ( chargeController.battery < 24.0 ) {
+      Serial.println('Battery voltage is low!');
+    } else {
+      Serial.println('Battery voltage is normal.');
+    }
+
+  } else {
+    Serial.println("failed to update");
+  }
+
+  delay(5000);
+}
